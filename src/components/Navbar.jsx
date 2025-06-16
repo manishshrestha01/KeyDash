f6f460d46f51ff3b215b59a0fffb2b5a6533644b
manishshrestha01 <shresthamanish8080@gmail.com>
2025-06-16 23:54:06 +0545

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="images/logo.svg"
+            src="src/images/logo.svg"
             className="h-8"
             alt="logo"
           />
