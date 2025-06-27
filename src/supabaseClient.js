bd82a73fe573959a172e580a7779b558652f6689
manishshrestha01 <shresthamanish8080@gmail.com>
2025-06-27 17:07:44 +0545

    supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
-// src/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://uiggrkayyxcrsovzrrbn.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZ2dya2F5eXhjcnNvdnpycmJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwMTUzNzgsImV4cCI6MjA2NjU5MTM3OH0.n5F-ytx_3FfPExXy9SPCGQSLZDrwywROwcF4IcY5t9s'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
