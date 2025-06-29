830ada3cd8d41f179f42485b4fab0936982570c8
manishshrestha01 <shresthamanish8080@gmail.com>
2025-06-29 22:02:16 +0545

    profile avatar fix

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -131,8 +131,8 @@ const Profile = () => {
             className="w-14 h-14 rounded-full object-cover border-2 border-yellow-400"
           />
         ) : (
-          <div className="bg-yellow-400 text-black font-bold rounded-full w-14 h-14 flex items-center justify-center text-xl">
-            {profile.display_name?.charAt(0).toUpperCase() || "U"}
+          <div className="w-14 h-14 bg-gray-600 flex items-center justify-center rounded-full border-2 border-yellow-400">
+            <svg className="w-12 h-12 rounded-full object-cover" fill="currentColor" viewBox="0 0 24 24"><path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
           </div>
         )}
         <div>
