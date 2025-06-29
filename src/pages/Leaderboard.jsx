f2d9185c9f15f82cdcd690bb9b44a7bac775c70e
manishshrestha01 <shresthamanish8080@gmail.com>
2025-06-29 20:17:56 +0545

    default avatar fixed in leaderboard

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -132,7 +132,11 @@ const Leaderboard = () => {
                     />
                   ) : (
                     <div className="w-8 h-8 bg-gray-600 flex items-center justify-center rounded-full">
-                      <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 24 24">
+                      <svg
+                        className="w-8 h-8 rounded-full object-cover"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
                         <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                       </svg>
                     </div>
