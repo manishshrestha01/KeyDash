432ee1358013f15fbdafa4f46217872669897e85
manishshrestha01 <shresthamanish8080@gmail.com>
2025-06-30 09:01:12 +0545

    white border in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -128,10 +128,10 @@ const Profile = () => {
           <img
             src={profile.avatar_url}
             alt="Avatar"
-            className="w-14 h-14 rounded-full object-cover border-2 border-yellow-400"
+            className="w-14 h-14 rounded-full object-cover border-2 border-white"
           />
         ) : (
-          <div className="w-14 h-14 bg-gray-600 flex items-center justify-center rounded-full border-2 border-yellow-400">
+          <div className="w-14 h-14 bg-gray-600 flex items-center justify-center rounded-full border-2 border-white">
             <svg className="w-12 h-12 rounded-full object-cover" fill="currentColor" viewBox="0 0 24 24"><path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
           </div>
         )}
